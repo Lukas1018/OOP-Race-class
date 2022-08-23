5292ee865835efe54488e76b5f39f8f3e6808206
Lukas1018 <lukasvelzys@gmail.com>
2022-08-23 18:17:32 +0300

    add fuction to draw cars

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// susirenkam vartotojo input +
+// susikuriam Car klasę +
+// nusipiešiam auto ir finish line
+// nustatom kiekvieno auto greitį
+// judinam (move) kiekviena auto
+// tikrinam win case scenario
+// pridedam winner text
+// pridedam info text
+// pakeičiam div į auto img
+// pakeičiam div į finish line img
+// jei reikės -> fixes ir updates
+// extra task - konvertuoti ES6 į ES5 sintaksę
+// todo darysim su git ir git commitais
+
 let numberOfCars;
 let distance;
 let cars = [];
@@ -6,12 +20,12 @@ let cars = [];
 
 function getUserInput() {
   do {
-    numberOfCars = prompt("Enter number of cars", 2);
+    numberOfCars = Number(prompt("Enter number of cars", 5));
   } while (numberOfCars < 0);
 
   do {
-    distance = prompt("Enter distance", 100);
-  } while (distance < 0);
+    distance = Number(prompt("Enter distance", 100));
+  } while (!Number.isInteger(distance) && distance > 0);
 }
 
 // 2
@@ -33,16 +47,26 @@ class Car {
   }
 }
 
-// 3
-// 3.1
+// 3 function drawCars(numberOfCars) kuri nupiesia tiek auto kiek ivesta userio.
+// 3.1 susikuriam tuscia array globaliai cars i kuri supushinsim tiek objektu, kiek useris ivede auto lenktyniauti;
+// kurti panaudojant klase Car ir sukti loopa kol pasieksim numberOfCars;
+// kaip parametra i Car klase, paduoti array indexa (i);
 
 const drawCars = (amount) => {
   for (let i = 0; i < amount; i++) {
     cars.push(new Car(i));
   }
+  cars.forEach((car) => {
+    const newCar = document.createElement("div");
+    newCar.classList.add("car");
+    newCar.style.top = 60 * car.id + "px";
+    document.body.append(newCar);
+  });
 };
 
-//3.2
+//3.2 sukt loopa per visus car array objektus ir kiekvienam is ju:
+// sukurti nauja HTML elementa div, prideti klase 'car', nurodyti pozicija per .style.top = 60 * car.id + 'px'
+// ir appendinti i dokumenta.
 
 getUserInput();
 drawCars(numberOfCars);
